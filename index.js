7a694506dedc653bd8600f99a2571937928fd583
Rocky Dey <coderrocky25@gmail.com>
2024-04-27 15:19:24 +0600

    delete api complete

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/all-spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
