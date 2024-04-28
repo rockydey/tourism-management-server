07d30432228099f76176e323bd40a4f3ba5295e3
Rocky Dey <coderrocky25@gmail.com>
2024-04-28 13:46:53 +0600

    firebase link added to cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://tourism-management-auth.web.app"],
   })
 );
 app.use(express.json());
