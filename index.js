806b42e2d9da502ec371d5d9381edb224bc493e7
Rocky Dey <coderrocky25@gmail.com>
2024-04-28 15:11:17 +0600

    firebase domain updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tourism-management-auth.web.app"],
+    origin: ["http://localhost:5173", "https://tourism-management-auth.web.app", "https://tourism-management-auth.firebaseapp.com"],
   })
 );
 app.use(express.json());
